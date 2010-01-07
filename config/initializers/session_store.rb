2e676a5dc9d801352bd86ed702762915c69b3941
Scott Woods <scott@westarete.com>
2010-01-07 08:06:44 -0500

    Use PROJECT for something to easily find/replace with the real project name.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_rails-template_session',
-  :secret      => 'db4ec1c450024d4a71e0e3f03b9e83dbda620576b49ba9a01d75fe1ec57254647aca038855e36e607b8855bc2ceae88f93d6499f7cef331e782b441234b88153'
+  :key         => '_PROJECT_session',
+  :secret      => 'development secret'
 }
 
 # Use the database for sessions instead of the cookie-based default,
